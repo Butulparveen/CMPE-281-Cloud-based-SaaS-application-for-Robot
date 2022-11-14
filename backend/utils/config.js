b9f0e27c2d5c646b1c1d5b8c4afba2537fa828dc
Kanupriya Agrawal <kanupriyasanjay.agrawal@sjsu.edu>
2022-11-14 14:42:25 -0800

    db config

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -2,10 +2,10 @@
 const config = {
   secret: "Your passport secret key",
   frontendURI: "http://localhost:3000",
-  mysqlUser: "root",
-  mysqlPassword: "password",
-  mysqlHost: "localhost",
-  mysqlDatabase: "281_project",
+  mysqlUser: "admin",
+  mysqlPassword: "admin123",
+  mysqlHost: "cmpe281awsdb.cdzcycgwsux2.us-west-2.rds.amazonaws.com",
+  mysqlDatabase: "team24Robot",
   mongoDBURI:
     "mongodb+srv://root:robotdb123@cluster0.tr7upgr.mongodb.net/cloudcomputing?retryWrites=true&w=majority",
     // "mongodb+srv://mobileCloudUser:cloudcomputing@cluster0.knqrf.mongodb.net/MobileRobotCloud?retryWrites=true&w=majority",
